cc38b3a8519706110a0968efd0af7d74f9f13c0a
Rishabh Srivastava <118589236+RishabhSri17@users.noreply.github.com>
2023-05-14 18:52:53 +0530

    Add files via upload

---
+++ teamdata.js
@@ -0,0 +1,468 @@
+const data = [
+	{
+		timestamp: '2022-11-14T08:34:22.229Z',
+		name: 'Aaryansh Pathak',
+		year: '2nd',
+		vertical: 'Video editor',
+		linkedinProfile: 'https://www.linkedin.com/in/aaryansh-pathak-424786241',
+		instagramProfile:
+			'https://www.instagram.com/invites/contact/?i=dryk86n0rpd7&utm_content=10i9v6w',
+		photo: 'https://drive.google.com/open?id=1eDAfhchaJu1ri3WupNOW3f68Ljlkh9aB',
+	},
+	{
+		timestamp: '2022-11-14T09:36:03.360Z',
+		name: 'Akhilendra Pratap',
+		year: '2nd',
+		vertical: 'Web Developer',
+		linkedinProfile: 'https://www.linkedin.com/in/akhilendrapratap',
+		instagramProfile: 'https://www.instagram.com/akhilendra_11/',
+		twitterProfile: 'https://twitter.com/Akhilendra112',
+		githubProfile: 'https://github.com/Akhilendra01',
+		photo: 'https://drive.google.com/open?id=1AkO-b4Tw3qLmHqgHe3sU_RBTZLq3OLhs',
+	},
+	{
+		timestamp: '2022-11-13T14:39:26.439Z',
+		name: 'Akshat Maheshwari',
+		year: '2nd',
+		vertical: 'Executive ',
+		linkedinProfile: 'https://www.linkedin.com/in/akshat-maheshwari-b5846b222',
+		instagramProfile: 'https://instagram.com/akrules03?igshid=YmMyMTA2M2Y=',
+		facebookProfile: 'https://www.facebook.com/profile.php?id=100083009631092',
+		twitterProfile:
+			'https://twitter.com/Akrules03?s=20&t=WH0JqQnvrEnNd3MI1Dbp2Q',
+		githubProfile: 'github.com/akrules03',
+		photo: 'https://drive.google.com/open?id=1ehSxZt6zBYEfmZYNS8lD-ja4zUZunWWj',
+	},
+	{
+		timestamp: '2022-11-13T14:09:16.582Z',
+		name: 'Akshit Sharma',
+		year: '2nd',
+		vertical: 'Executive',
+		linkedinProfile: 'linkedin.com/in/akshit-sharma-b70610223',
+		instagramProfile: 'https://www.instagram.com/akshittsharmaa/',
+		twitterProfile: 'https://twitter.com/AkshittSharmaa',
+		photo: 'https://drive.google.com/open?id=1jk74pggA_EurkTRbntSrEJFtJBzSg-iX',
+	},
+	{
+		timestamp: '2022-11-13T17:58:54.923Z',
+		name: 'Aniket Mishra',
+		year: '2nd',
+		vertical: 'Video Editor',
+		linkedinProfile: 'https://www.linkedin.com/in/aniket-mishra-721370129',
+		photo: 'https://drive.google.com/open?id=1O3NUi7wcNsdrosiDZdh2ID7TKIkYnWvz',
+	},
+	{
+		timestamp: '2022-11-13T10:54:30.168Z',
+		name: 'Anjaneya Dwivedi',
+		year: '2nd',
+		vertical: 'Executive',
+		linkedinProfile: 'https://www.linkedin.com/in/anjaneya-dwivedi-0b755a201',
+		
+	},
+	{
+		timestamp: '2022-11-13T15:45:53.727Z',
+		name: 'Anmol Singh',
+		year: '2nd',
+		vertical: 'Executive',
+		linkedinProfile: 'http://linkedin.com/in/anmol-singh-b094b3244',
+		instagramProfile: 'https://instagram.com/anmol12_singh?igshid=YmMyMTA2M2Y=',
+		facebookProfile: 'https://www.facebook.com/profile.php?id=100076607132656',
+		photo: 'https://drive.google.com/open?id=1wtMbguaiKrTYXTvdSqQWLPO0tUS49W4h',
+	},
+	{
+		timestamp: '2022-11-13T11:03:22.476Z',
+		name: 'Anshika Arjariya ',
+		year: '2nd',
+		vertical: 'Content Writer',
+		linkedinProfile: 'https://www.linkedin.com/in/anshika-arjariya-18a911244',
+		instagramProfile: 'anshikaarjariya ',
+		photo: 'https://drive.google.com/open?id=14oO3CclkMWWNBuhyjwbJGKw3Gcm1pL_C',
+	},
+	{
+		timestamp: '2022-11-13T16:34:25.128Z',
+		name: 'Anurag Soni',
+		year: '2nd',
+		vertical: 'Graphic Designer',
+		linkedinProfile: 'Anurag Soni',
+		instagramProfile: 'anuragsoni.as',
+		
+	},
+	{
+		name: 'Gautam Pratap',
+		year: '2nd',
+		vertical: 'Graphic Designer',
+		linkedinProfile: 'https://www.linkedin.com/in/gautampratap',
+		instagramProfile: 'https://www.instagram.com/yoursgautampratap/',
+		
+	},
+	{
+		timestamp: '2022-11-13T18:19:21.830Z',
+		name: 'Arin Bansal',
+		year: '2nd',
+		vertical: 'Executive ',
+		linkedinProfile:
+			'https://www.google.com/url?sa=t&source=web&rct=j&url=https://in.linkedin.com/in/arin-bansal-46109a227&ved=2ahUKEwjxyNCB4qv7AhVw7TgGHTQdCnEQjjh6BAgUEAE&usg=AOvVaw3QKYbeCVAnvYA9rNuIP4in',
+		instagramProfile:
+			'https://www.google.com/url?sa=t&source=web&rct=j&url=https://www.instagram.com/arin_bansal/%3Fhl%3Den&ved=2ahUKEwij25mW4qv7AhW9xTgGHXJZC8QQFnoECBYQAQ&usg=AOvVaw2Sv8ANIjxM9RjZoLlLIaZu',
+		photo: 'https://drive.google.com/open?id=1xlhhNeVKQStKtXoF5YLR0gnNdAG2N888',
+	},
+	{
+		timestamp: '2022-11-13T14:25:21.297Z',
+		name: 'Arnika Chaturvedi',
+		year: '2nd',
+		vertical: 'Executive',
+		linkedinProfile: 'https://www.linkedin.com/in/arnika-chaturvedi-03444a237',
+		instagramProfile:
+			'https://instagram.com/arnika_chaturvedi?igshid=YmMyMTA2M2Y=',
+		photo: 'https://drive.google.com/open?id=1vKh7ZfK8V1l0h4W8P0bkauESlaFtVgFs',
+	},
+	{
+		timestamp: '2022-11-14T09:37:31.675Z',
+		name: 'Arpit Gupta',
+		year: '2nd',
+		vertical: 'Photographer ',
+		linkedinProfile: 'https://www.linkedin.com/in/arpit-gupta-24170b238',
+		instagramProfile: 'https://instagram.com/_.arpit_.01?igshid=YmMyMTA2M2Y=',
+		twitterProfile:
+			'https://twitter.com/ArpitGu72813231?t=-cS7mVb-Mlj78fK2e9zrYw&s=09',
+		photo: 'https://drive.google.com/open?id=1jQF59iYZ1rFPr-GKfKwzl-2ZnzRs3FCA',
+	},
+	{
+		timestamp: '2022-11-13T17:32:14.725Z',
+		name: 'Ashay Jain ',
+		year: '2nd',
+		vertical: 'Executive ',
+		linkedinProfile: 'https://www.linkedin.com/in/ashay-jain-485a43244',
+		instagramProfile: 'easyyy_10',
+		
+	},
+	{
+		timestamp: '2022-11-13T14:24:02.901Z',
+		name: 'Asmita Jain',
+		year: '2nd',
+		vertical: 'Executive ',
+		linkedinProfile: 'https://www.linkedin.com/in/asmita-jain-a20343239',
+		instagramProfile: 'https://instagram.com/asmita_jain03?igshid=YmMyMTA2M2Y=',
+		photo: 'https://drive.google.com/open?id=1ZjvVtVL8L2v8jAdAObypLlVZsxcAmgvX',
+	},
+	{
+		timestamp: '2022-11-13T12:05:46.160Z',
+		name: 'Ayushi Verma ',
+		year: '2nd',
+		vertical: 'Executive ',
+		linkedinProfile: 'https://www.linkedin.com/in/ayushi-verma-318a10244',
+		instagramProfile: 'https://instagram.com/ayushiii_v?igshid=YmMyMTA2M2Y=',
+		facebookProfile: 'https://www.facebook.com/profile.php?id=100083007799113',
+		photo: 'https://drive.google.com/open?id=1OGLaVQ7APqUmiZxW8t-r55d_wvHxMZrX',
+	},
+	{
+		timestamp: '2022-11-13T14:05:55.377Z',
+		name: 'Bharat Verma ',
+		year: '2nd',
+		vertical: 'Executive and Content Writer ',
+		linkedinProfile: 'https://www.linkedin.com/in/bharat-verma-385284243',
+		instagramProfile: 'https://instagram.com/cheeee_ku?igshid=YmMyMTA2M2Y=',
+		facebookProfile: 'https://www.facebook.com/profile.php?id=100032990748860',
+		
+	},
+	{
+		timestamp: '2022-11-13T10:36:43.379Z',
+		name: 'Bhumika Chouhan',
+		year: '2nd',
+		vertical: 'Executive and Content writer ',
+		linkedinProfile: 'https://www.linkedin.com/in/bhumika-chouhan-237906237',
+		photo: 'https://drive.google.com/open?id=1avHA3CHkP0I7mkMkLwNaystx_s41aYY1',
+	},
+	{
+		timestamp: '2022-11-13T14:24:19.693Z',
+		name: 'Disha Badetia',
+		year: '2nd',
+		vertical: 'Executive',
+		linkedinProfile: 'https://www.linkedin.com/in/disha-badetia-337aa3242/',
+		instagramProfile: 'https://www.instagram.com/disha.1574/',
+		photo: 'https://drive.google.com/open?id=1QkEsrtI8-dbCTORr8JcgPm4IjVFRbRug',
+	},
+	{
+		timestamp: '2022-11-14T10:39:04.008Z',
+		name: 'Jayant Dhakad',
+		year: '2nd',
+		vertical: 'Web Developer',
+		linkedinProfile: 'https://www.linkedin.com/in/jayant-dhakad-8a5517233/',
+		instagramProfile: 'https://www.instagram.com/jayant_dhakad.1/',
+		githubProfile: 'https://github.com/jake8989',
+		photo: 'https://drive.google.com/open?id=1zl6VlaMgD7QmY5GNiiJEQL7qJq8LjrQM',
+	},
+	{
+		timestamp: '2022-11-13T10:00:11.868Z',
+		name: 'Kartik Tiwari',
+		year: '2nd',
+		vertical: 'Executive',
+		linkedinProfile: 'https://www.linkedin.com/in/kartik-tiwari-954b54244',
+		githubProfile: 'Kartik79',
+		photo: 'https://drive.google.com/open?id=1lqUcOANVgHQIPPIdnIVakWWQ_dt9DAZl',
+	},
+	{
+		timestamp: '2022-11-13T10:49:22.120Z',
+		name: 'Kartikeya Mande',
+		year: '2nd',
+		vertical: 'Executive ',
+		linkedinProfile: 'https://www.linkedin.com/in/kartikeya-mande-a5a006225',
+		instagramProfile:
+			'https://instagram.com/kartikeyamande19?igshid=YmMyMTA2M2Y=',
+		facebookProfile: 'https://www.facebook.com/kartikeya.mande',
+		githubProfile: 'https://github.com/Kartikeya192002',
+		
+	},
+	{
+		timestamp: '2022-11-13T18:05:24.580Z',
+		name: 'Kushagra Patel',
+		year: '2nd',
+		vertical: 'Photographer',
+		linkedinProfile: 'https://www.linkedin.com/in/kushagra-patel-43185b22b',
+		instagramProfile: 'kushagra_amicable',
+		facebookProfile: 'https://www.facebook.com/profile.php?id=100086292642370',
+		twitterProfile: 'None',
+		githubProfile: 'None',
+		photo: 'https://drive.google.com/open?id=1_h7TLeTUYgMmb8r7nAVS8KMd-b8CIlUl',
+	},
+	{
+		timestamp: '2022-11-13T16:42:29.181Z',
+		name: 'Lokesh Pawar',
+		year: '2nd',
+		vertical: 'Video Editor',
+		linkedinProfile: 'https://www.linkedin.com/in/wholokeshpawar/',
+		instagramProfile: 'https://www.instagram.com/lokesh.pawar.5/',
+		facebookProfile: 'https://www.facebook.com/lokesh.pawar.9484941/',
+		twitterProfile: 'https://mobile.twitter.com/lokeshpawar51',
+		photo: 'https://drive.google.com/open?id=15WFPyElcyBt--uB5OJLhNQJKzQf4RSj9',
+	},
+	{
+		timestamp: '2022-11-13T17:58:57.765Z',
+		name: 'Muzaina Tauqeer',
+		year: '2nd',
+		vertical: 'Executive',
+		linkedinProfile: 'https://www.linkedin.com/in/muzaina-tauqeer-5a263b245',
+		instagramProfile: 'muzaina_tauqeer',
+		facebookProfile: 'https://www.facebook.com/profile.php?id=100080462827438',
+		twitterProfile: '-',
+		githubProfile: '-',
+		photo: 'https://drive.google.com/open?id=170826QDTGunpbxDICv7AWkWO3ZF0OvjB',
+	},
+	{
+		timestamp: '2022-11-13T09:22:32.338Z',
+		name: 'Navneethan Pillai ',
+		year: '2nd',
+		vertical: 'Executive ',
+		linkedinProfile: 'https://www.linkedin.com/in/navneethan-pillai-ab9b53229',
+		instagramProfile: '_._navneethan_.___',
+		facebookProfile: 'https://www.facebook.com/navneethan.pillai',
+		twitterProfile:
+			'https://twitter.com/Navneethan_5320?t=pvKTn0cZ6MjT2SuHXbXhzQ&s=08',
+		photo: 'https://drive.google.com/open?id=1PWGtuyoEweIw_c85sF1uVB9A2HW4qnXI',
+	},
+	{
+		timestamp: '2022-11-13T11:26:03.238Z',
+		name: 'Rajkumar Majhi ',
+		year: '2nd',
+		vertical: ' Graphic Designer',
+		linkedinProfile: 'https://www.linkedin.com/in/rajkumar-majhi-1278b5226',
+		instagramProfile: 'https://www.instagram.com/art_by_rajkumar/',
+		facebookProfile:
+			'https://m.facebook.com/rajkumar.majhi.5473894?eav=AfbphBhB888-yGDiqJl4t4-QfpENKHJKT0WY0Xmr9X35cYVqp6U_BnfL3GZAbsSHodU&ref_component=mbasic_home_header&ref_page=%2Fwap%2Fhome.php&refid=8&paipv=0',
+		twitterProfile:
+			'https://twitter.com/Rajkumar__majhi?t=gL5nThuKTIRCqGlmiJC2Rg&s=09',
+		githubProfile: 'https://github.com/rajkumarmajhi31',
+		photo: 'https://drive.google.com/open?id=18gq2S_oNTloJ_d_ma0scDRviKkqvC56b',
+	},
+	{
+		timestamp: '2022-11-14T04:58:21.282Z',
+		name: 'Rama Bhala',
+		year: '2nd',
+		vertical: 'Executive ',
+		linkedinProfile: 'https://www.linkedin.com/in/rama-bhala-84143a224',
+		photo: 'https://drive.google.com/open?id=1ArN_JxLgjoXR3giHCxpLWs65VRcaGdOJ',
+	},
+	{
+		timestamp: '2022-11-13T09:48:04.673Z',
+		name: 'Rudraksh',
+		year: '2nd',
+		vertical: 'Graphic Designer',
+		linkedinProfile: 'https://www.linkedin.com/in/rudraksh-rajput-60249b244',
+		instagramProfile: 'rudr_aksh2002',
+		photo: 'https://drive.google.com/open?id=1Uocxd82St3FVYWyHfmFgU1pYgr_9UhsW',
+	},
+	{
+		timestamp: '2022-11-13T16:43:13.570Z',
+		name: 'Saiyam Saxena',
+		year: '2nd',
+		vertical: 'Executive',
+		linkedinProfile: 'https://www.linkedin.com/in/saiyam-saxena-b28961241/',
+		instagramProfile: 'https://www.instagram.com/saiyam_saxena/',
+		facebookProfile: 'https://www.facebook.com/profile.php?id=100007095016924',
+		twitterProfile: 'https://twitter.com/SaiyamSaxena',
+		githubProfile: 'https://github.com/Saiyam10',
+		photo: 'https://drive.google.com/open?id=1kn0xPtbxFJYUgdUuu-Q6yKQBnIV0oQSz',
+	},
+	{
+		timestamp: '2022-11-13T14:33:41.046Z',
+		name: 'Samyukta alawa',
+		year: '2nd',
+		vertical: 'Executive ',
+		linkedinProfile: 'https://www.linkedin.com/in/samyukta-alawa-1785ba245',
+		instagramProfile: ' _samyukta_25',
+		facebookProfile: '-',
+		twitterProfile: '-',
+		githubProfile: '-',
+		
+	},
+	{
+		timestamp: '2022-11-13T11:00:57.770Z',
+		name: 'Sania Zehra Rizvi ',
+		year: '2nd',
+		vertical: 'Executive ',
+		linkedinProfile: 'https://www.linkedin.com/in/sania-zehra-rizvi-6b7a95244',
+		instagramProfile: 'https://instagram.com/_.sa.n.ia._?igshid=YmMyMTA2M2Y=',
+		photo: 'https://drive.google.com/open?id=1hSJJZ6_RtbesLJrhIUaMSkH1ZNXFWt_J',
+	},
+	{
+		timestamp: '2022-11-14T08:33:38.058Z',
+		name: 'Sanidhya Vijay',
+		year: '2nd',
+		vertical: 'Graphic Designer',
+		linkedinProfile: 'https://www.linkedin.com/in/sanidhya-vijay-02b183229',
+		instagramProfile:
+			'https://www.instagram.com/invites/contact/?i=artry4wm730f&utm_content=22347mn',
+		photo: 'https://drive.google.com/open?id=1jSh43dQuRIUfE1sKW7pyqHmRd9zG9mwL',
+	},
+	{
+		timestamp: '2022-11-14T04:55:14.755Z',
+		name: 'Sanya Arora ',
+		year: '2nd',
+		vertical: 'Executive ',
+		linkedinProfile: 'https://www.linkedin.com/in/sanya-arora-228212241',
+		photo: 'https://drive.google.com/open?id=1LjxSVshH382ac28eF2N3mYuaoQYNOXRy',
+	},
+	{
+		timestamp: '2022-11-13T21:02:46.815Z',
+		name: 'Shailee Gavnekar ',
+		year: '2nd',
+		vertical: 'Executive ',
+		linkedinProfile: 'https://www.linkedin.com/in/shailee-gavnekar-32b780233',
+		instagramProfile:
+			'https://instagram.com/shailee_gavnekar27?igshid=YmMyMTA2M2Y=',
+		facebookProfile: 'https://www.facebook.com/profile.php?id=100083231649592',
+		twitterProfile:
+			'https://twitter.com/GavnekarShailee?t=SOxijlZ8BsR6knG3eFU3Dg&s=08',
+		photo: 'https://drive.google.com/open?id=1ci1KvIxcPNDnEMQ5TrFgShqC1spv1BAl',
+	},
+	{
+		timestamp: '2022-11-13T12:09:26.871Z',
+		name: 'Suhaani Batra',
+		year: '2nd',
+		vertical: 'Executive',
+		linkedinProfile: 'https://www.linkedin.com/in/suhaani-batra-842858244/',
+		instagramProfile: 'https://www.instagram.com/suhaanii.batra/',
+		facebookProfile: 'https://www.facebook.com/profile.php?id=100082825976960',
+		twitterProfile: 'https://twitter.com/BatraSuhaani',
+		githubProfile: 'https://github.com/suhaanibatra',
+		photo: 'https://drive.google.com/open?id=1ajCMFQgeHtMMS_z_Ossra4rxDp9E1rME',
+	},
+	{
+		timestamp: '2022-11-13T14:14:38.134Z',
+		name: 'Suhani Jain ',
+		year: '2nd',
+		vertical: 'Executive ',
+		linkedinProfile: 'https://www.linkedin.com/in/suhani-jain-b96800239',
+		instagramProfile: 'https://instagram.com/accio_oo9?igshid=YmMyMTA2M2Y=',
+		facebookProfile: 'https://www.facebook.com/profile.php?id=100082908894329',
+		twitterProfile:
+			'https://twitter.com/Accio5406?t=WpN-jTH8WSjeOILYm_ud-Q&s=09',
+		photo: 'https://drive.google.com/open?id=12W2ESEJgY90xWVdwX7UpxaR7rCwJi9DY',
+	},
+	{
+		timestamp: '2022-11-13T09:31:02.305Z',
+		name: 'Suyash Tiwari ',
+		year: '2nd',
+		vertical: 'Executive ',
+		linkedinProfile: 'https://www.linkedin.com/in/suyash-tiwari-8b1910244',
+		instagramProfile: 'suyash09tiwari',
+		facebookProfile: 'Suyash Tiwari ',
+		photo: 'https://drive.google.com/open?id=188R4GISphXtnqN3LxjJfvNoErHswYMyw',
+	},
+	{
+		timestamp: '2022-11-13T14:32:21.947Z',
+		name: 'Uday Pratap Singh ',
+		year: '2nd',
+		vertical: 'Executive ',
+		linkedinProfile: 'https://www.linkedin.com/in/uday-pratap-singh-857869229',
+		instagramProfile: 'the_bfg__',
+		facebookProfile: 'https://www.facebook.com/profile.php?id=100083295217122',
+		photo: 'https://drive.google.com/open?id=13iHvYY7zFcZfCjjNFanYj5JPWRAIRtas',
+	},
+	{
+		timestamp: '2022-11-13T22:46:27.344Z',
+		name: 'Vidit Purohit ',
+		year: '2nd',
+		vertical: 'Executive ',
+		linkedinProfile: 'https://www.linkedin.com/in/vidit-purohit-36b538231',
+		instagramProfile:
+			'https://instagram.com/vidit01purohit?igshid=YmMyMTA2M2Y=',
+		
+	},
+	{
+		timestamp: '2022-11-13T10:17:01.027Z',
+		name: 'Zoheb Hasan ',
+		year: '2nd',
+		vertical: 'Executive ',
+		linkedinProfile: 'https://www.linkedin.com/in/zoheb-hasan-74a35a231',
+		facebookProfile: 'https://www.facebook.com/zoheb.hasan.106',
+		photo: 'https://drive.google.com/open?id=11RI24AF463XQ5SawsacXnvwHjQyS_X8s',
+	}
+	
+];
+
+
+
+var icons = [
+	{ header: 'linkedin', icon: 'linkedin-icon.png' },
+	{ header: 'insta', icon: 'insta-icon.png' },
+	{ header: 'fb', icon: 'fb-icon.png' },
+	{ header: 'twitter', icon: 'twitter-icon.jpeg' },
+	{ header: 'github', icon: 'github-icon.png' },
+  ];
+  
+
+
+
+function createTableFromJsonData() {
+    
+    var headers = Object.keys(data[2]);
+  
+    var headerRowHTML = '<tr>';
+    for (var i = 0; i < headers.length; i++) {
+        headerRowHTML += '<th>' + headers[i] + '</th>';
+    }
+    headerRowHTML += '</tr>';
+  
+    var allRecordsHTML = '';
+    for (var i = 0; i < data.length; i++) {
+        allRecordsHTML += '<tr>';
+        for (var j = 0; j < headers.length; j++) {
+            var header = headers[j];
+			var icon = icons.find(function(obj) { return obj.header === header; });
+            if (header === 'href') {
+				allRecordsHTML += '<td><a href="' + data[i][header] + '"><img height="20px" src="' + icon.icon + '" alt="' + icon.header.toUpperCase() + '"></a></td>';
+			} else if (header === 'image') {
+                allRecordsHTML += '<td><img src="' + data[i][header] + '"></td>';
+            } else {
+                allRecordsHTML += '<td>' + data[i][header] + '</td>';
+            }
+        }
+        allRecordsHTML += '</tr>';
+    }
+  
+    var table = document.querySelector('#display_json_data');
+    table.innerHTML = headerRowHTML + allRecordsHTML;
+}
+  
+window.onload = createTableFromJsonData;
